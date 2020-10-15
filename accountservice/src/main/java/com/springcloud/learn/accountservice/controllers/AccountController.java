41b829a47b81561ac883adb15d08209ca444193e
Prabhaker Annarapu <pannarap@opentext.com>
2020-10-15 09:11:57 +0530

    new user service

---
+++ accountservice/src/main/java/com/springcloud/learn/accountservice/controllers/AccountController.java
@@ -0,0 +1,16 @@
+package com.springcloud.learn.accountservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping
+    public String getAccount() {
+
+        return "Hello Account .. !!";
+    }
+}
---
+++ userservice/src/test/java/com/spring/cloud/learn/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.learn.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
